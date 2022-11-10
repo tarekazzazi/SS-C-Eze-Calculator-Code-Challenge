a9f8dfda522b1a73a4117058de15b595d4a16dd5
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-11-10 16:59:47 -0600

    Does Additon

--- server/public/client/client.js
+++ server/public/client/client.js
@@ -1,5 +1,30 @@
 $(onReady);
 
+let setOperator;
+
 function onReady() {
   console.log("Jquery loaded");
+  $("#Add").on("click", DoAddition);
+}
+
+function DoAddition(event) {
+  event.preventDefault();
+  setOperator = "+";
+  console.log("DOING MATH");
+  DoMath();
+}
+
+let Additon = 0;
+// Eventually move this to server side
+function DoMath() {
+  console.log("Operator is", setOperator);
+  switch (setOperator) {
+    case "+":
+      Additon = Number(Additon) + Number($("#first-input").val());
+      console.log(Additon);
+      break;
+    default:
+      console.log("oh no I didnt get the operator");
+      break;
+  }
 }
