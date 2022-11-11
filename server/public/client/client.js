509dfd8bf1d21d7df0591d6dc9210968474e6085
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-11-10 18:29:58 -0600

    added display of addition results

--- server/public/client/client.js
+++ server/public/client/client.js
@@ -14,14 +14,23 @@ function DoAddition(event) {
   DoMath();
 }
 
-let Additon = 0;
 // Eventually move this to server side
+let newNumber = 0;
+let Inputval = 0;
+
 function DoMath() {
-  console.log("Operator is", setOperator);
   switch (setOperator) {
     case "+":
-      Additon = Number(Additon) + Number($("#first-input").val());
-      console.log(Additon);
+      // Variables
+      newNumber = newNumber + Inputval;
+      Inputval = Number($("#first-input").val());
+
+      // way of displaying results
+      const res = newNumber + Inputval;
+      console.log(`${newNumber} + ${Inputval} = ${res}`);
+
+      //  Clears Inputs
+      $("#first-input").val("");
       break;
     default:
       console.log("oh no I didnt get the operator");
