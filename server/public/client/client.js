d104375d56441358aab276a553ac48a22baddafe
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-11-12 20:16:27 -0600

    math works but has to be typed out

--- server/public/client/client.js
+++ server/public/client/client.js
@@ -6,13 +6,24 @@ let input;
 function onReady() {
   console.log("Jquery loaded");
   $("#plus").on("click", sendDoAddition);
+  $("#minus").on("click", sendDoSubtraction);
+  $("#times").on("click", sendDoMultiplycation);
+  $("#divide").on("click", sendDoDivison);
   $("#clear").on("click", clearInputs);
-  $();
+  $("#submitBtn").on("click", getResults);
 }
 
 function clearInputs(event) {
   event.preventDefault();
-  input;
+  // resets input value so it starts at the first num entered not the answer from =
+  input = undefined;
+  Number($("#input1").val("0"));
+}
+
+function getResults(event) {
+  event.preventDefault();
+  DoMath();
+  Number($("#input1").val(input));
 }
 
 function sendDoAddition(event) {
@@ -20,6 +31,44 @@ function sendDoAddition(event) {
   event.preventDefault();
   setOperator = "+";
   console.log(setOperator);
+  // Checks if a number is left over from pressing equals and emptys the value so it dosent do the operation by itself
+  if (input !== undefined) {
+    $("#input1").val("");
+  }
+  // actually does the math
+  DoMath();
+}
+
+function sendDoSubtraction(event) {
+  //  Set Operator to subtraction
+  event.preventDefault();
+  setOperator = "-";
+  console.log(setOperator);
+  if (input !== undefined) {
+    $("#input1").val("");
+  }
+  DoMath();
+}
+
+function sendDoMultiplycation(event) {
+  //  Set Operator to Multiplycation
+  event.preventDefault();
+  setOperator = "*";
+  console.log(setOperator);
+  if (input !== undefined) {
+    $("#input1").val("");
+  }
+  DoMath();
+}
+
+function sendDoDivison(event) {
+  //  Set Operator to Divison
+  event.preventDefault();
+  setOperator = "/";
+  console.log(setOperator);
+  if (input !== undefined) {
+    $("#input1").val("");
+  }
   DoMath();
 }
 
@@ -31,18 +80,20 @@ function DoMath() {
       if (input === undefined) {
         input = Number($("#input1").val());
         $("#input1").val("");
+        console.log(input);
       } else {
-        input = Number($("#input1").val()) + input;
-        $("#input1").val("");
+        input = input + Number($("#input1").val());
+        console.log(input);
       }
       break;
     case "-":
       if (input === undefined) {
         input = Number($("#input1").val());
         $("#input1").val("");
+        console.log(input);
       } else {
-        input = Number($("#input1").val()) - input;
-        $("#input1").val("");
+        input = input - Number($("#input1").val());
+        console.log(input);
       }
       break;
     case "*":
@@ -50,8 +101,15 @@ function DoMath() {
         input = Number($("#input1").val());
         $("#input1").val("");
       } else {
-        input = Number($("#input1").val()) * input;
+        input = input * Number($("#input1").val());
+      }
+      break;
+    case "/":
+      if (input === undefined) {
+        input = Number($("#input1").val());
         $("#input1").val("");
+      } else {
+        input = input / Number($("#input1").val());
       }
       break;
     default:
